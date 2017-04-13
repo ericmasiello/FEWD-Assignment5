78b609b06237072dd94a2c29961601165f959f31
nanseay <nancy.seay@voxmedia.com>
2017-04-13 12:34:45 -0400

    Nancy Seay HW4

--- js/index.js
+++ js/index.js
@@ -1,11 +1,32 @@
 $(document).ready(function() {
 
-//basic syntax $(selector).action()
+  // hide all the extra text initially
+  $('.hide').hide();
 
-$('.readmore').click(event);
+  // click on either link to show text
+  $('.readmore').click(showOnClick);
+  $('.learnmore').click(learnMoreText);
 
-  function event() {
-    event.preventDefault();
+  // click to hide readmore
+  $('.readless').click(hideOnClick);
+
+  // show readmore
+  function showOnClick() {
+    $('.readmore').hide();
+    $('#showonclick').slideDown();
+    $('.readless').show();
+  }
+
+  // hide readless
+  function hideOnClick() {
+    $('.readless').slideUp ();
+    $('#showonclick').slideUp();
+    $('.readmore').show();
 }
+   // show learnmore
+  function learnMoreText() {
+    $('#learnmoretext').slideDown();
+    $('.learnmore').hide();
+  }
 
 });
