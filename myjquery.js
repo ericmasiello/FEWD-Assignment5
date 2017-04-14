b6a65a45888d77d11e4a5b86f3635ab486ef09d5
jeffwr12 <JEFF@JEFFs-Air.fios-router.home>
2017-04-14 18:07:36 -0400

    Assignment 4

--- myjquery.js
+++ myjquery.js
@@ -1,2 +1,39 @@
-myjquery.js
+$(document).ready(function() {
+	
+        // console.log('I got here!');
+
+
+
+$(".readmore").click(function(){
+
+ 	$(".hide").slideDown("slow");
+
+ 	$(".readmore").hide();
+
+ 	$(".learnmore").stop().slideDown();
+
+
+});
+
+$(".readless").click(function(){
+
+     $("#show-this-on-click").slideUp("slow");
+
+     $(".readless").hide();
+
+     $(".readmore").show();
+
+  });
+
+
+
+
+
+
+
+
+
+	/* put all your code here */
+	// this is the end of the document ready function
+});
 
