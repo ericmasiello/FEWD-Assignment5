1ba727232027aa943dd10a16ce10900d091684c7
jeffwr12 <JEFF@JEFFs-MacBook-Air.local>
2017-04-17 18:17:37 -0400

    Updates 4/17

--- myjquery.js
+++ myjquery.js
@@ -1,15 +1,32 @@
+
+
 $(document).ready(function() {
 	
         // console.log('I got here!');
 
+$('.readmore').click(function(event) {
+  // Do this first!
+  event.preventDefault();
+
+  // Then do your thing...
+});
+
 
 $(".readmore").click(function(){
+
 
  	$(".hide").slideDown();
 
  	$(".readmore").hide();
 
 	
+});
+
+$('.readless').click(function(event) {
+  // Do this first!
+  event.preventDefault();
+
+  // Then do your thing...
 });
 
 $(".readless").click(function(){
@@ -22,10 +39,20 @@ $(".readless").click(function(){
 
   });
 
+$('.learnmore').click(function(event) {
+  // Do this first!
+  event.preventDefault();
+
+
+});
+
+
+
 $(".learnmore").click(function(){
 
      $("#learnmoretext").slideDown();
 
+     $(".learnmore").hide();
     
 
   });
