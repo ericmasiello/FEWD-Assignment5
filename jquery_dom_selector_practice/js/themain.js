82977283060a879aa5edeb68856af12703c10afd
michael jones <mw.jones2009@gmail.com>
2018-04-24 09:46:02 -0400

    beautified

--- jquery_dom_selector_practice/js/themain.js
+++ jquery_dom_selector_practice/js/themain.js
@@ -1,18 +1,18 @@
-function readMore(){
+function readMore() {
   console.log('readmore');
-    //$('#show-this-on-click').css('display','block');
-    //$('#show-this-on-click').toggle("display");
-    $('.readmore').hide();
-    $('#show-this-on-click').slideDown(1500,function(){
-      console.log('slide down');
-      $('.readless').show();
-    })
+  //$('#show-this-on-click').css('display','block');
+  //$('#show-this-on-click').toggle("display");
+  $('.readmore').hide();
+  $('#show-this-on-click').slideDown(1500, function () {
+    console.log('slide down');
+    $('.readless').show();
+  })
 }
 
 function readLess() {
   console.log('readless');
   $('.readless').hide();
-  $('#show-this-on-click').slideUp(1500,function(){
+  $('#show-this-on-click').slideUp(1500, function () {
     console.log('slide up');
     $('.readmore').show();
   });
@@ -21,15 +21,15 @@ function readLess() {
 
 
 $('.readmore').on('click', readMore);
-$('.readless').on('click',readLess);
+$('.readless').on('click', readLess);
 
-function learnMore(){
+function learnMore() {
   console.log('learnmore');
-  $('#learnmoretext').slideDown(1500,function(){
+  $('#learnmoretext').slideDown(1500, function () {
     console.log('slide down');
     $('#learnmoretext').show();
     $('.learnmore').hide();
   })
 
 }
-$('.learnmore').on('click',learnMore);
+$('.learnmore').on('click', learnMore);
